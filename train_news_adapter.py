21aedcd3663b7d58584d5c2a26ccdfa60df0c27c
Atnafu Lambebo Tonja <atnafuatx@gmail.com>
2023-04-12 05:37:26 -0600

    update

--- train_news_adapter.py
+++ train_news_adapter.py
@@ -377,25 +377,32 @@ def load_and_cache_examples(args, tokenizer, labels, pad_token_label_id, mode):
         features = torch.load(cached_features_file)
     else:
         print("Creating features from dataset file at", args.data_dir)
-        examples = read_examples_from_file(args,args.data_dir, mode)
-        features = convert_examples_to_features(
-            examples,
-            labels,
-            args.max_seq_length,
-            tokenizer,
-            cls_token_at_end=bool(args.model_type in ["xlnet"]),
-            # xlnet has a cls token at the end
-            cls_token=tokenizer.cls_token,
-            cls_token_segment_id=2 if args.model_type in ["xlnet"] else 0,
-            sep_token=tokenizer.sep_token,
-            sep_token_extra=bool(args.model_type in ["roberta"]),
-            # roberta uses an extra separator b/w pairs of sentences, cf. github.com/pytorch/fairseq/commit/1684e166e3da03f5b600dbb7855cb98ddfcd0805
-            pad_on_left=bool(args.model_type in ["xlnet"]),
-            # pad on the left for xlnet
-            pad_token=tokenizer.convert_tokens_to_ids([tokenizer.pad_token])[0],
-            pad_token_segment_id=4 if args.model_type in ["xlnet"] else 0,
-            pad_token_label_id=pad_token_label_id,
-        )
+      #  logger.info("Creating features from dataset file at %s", args.data_dir)
+        instances = read_examples_from_file(args, args.data_dir, mode)
+        features = convert_examples_to_features(instances, tokenizer, labels, args.max_seq_length)
+        if args.local_rank in [-1, 0]:
+            logger.info("Saving features into cached file %s", cached_features_file)
+            torch.save(features, cached_features_file)
+        # print("Creating features from dataset file at", args.data_dir)
+        # examples = read_examples_from_file(args,args.data_dir, mode)
+        # features = convert_examples_to_features(
+        #     examples,
+        #     labels,
+        #     args.max_seq_length,
+        #     tokenizer,
+        #     cls_token_at_end=bool(args.model_type in ["xlnet"]),
+        #     # xlnet has a cls token at the end
+        #     cls_token=tokenizer.cls_token,
+        #     cls_token_segment_id=2 if args.model_type in ["xlnet"] else 0,
+        #     sep_token=tokenizer.sep_token,
+        #     sep_token_extra=bool(args.model_type in ["roberta"]),
+        #     # roberta uses an extra separator b/w pairs of sentences, cf. github.com/pytorch/fairseq/commit/1684e166e3da03f5b600dbb7855cb98ddfcd0805
+        #     pad_on_left=bool(args.model_type in ["xlnet"]),
+        #     # pad on the left for xlnet
+        #     pad_token=tokenizer.convert_tokens_to_ids([tokenizer.pad_token])[0],
+        #     pad_token_segment_id=4 if args.model_type in ["xlnet"] else 0,
+        #     pad_token_label_id=pad_token_label_id,
+        # )
         if args.local_rank in [-1, 0]:
             print("Saving features into cached file", cached_features_file)
             torch.save(features, cached_features_file)
