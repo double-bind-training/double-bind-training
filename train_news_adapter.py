9c4c5e11b2683a658138417e4ef20a1cd7c7d500
Atnafu Lambebo Tonja <atnafuatx@gmail.com>
2023-04-12 03:45:00 -0600

    update

--- train_news_adapter.py
+++ train_news_adapter.py
@@ -377,7 +377,7 @@ def load_and_cache_examples(args, tokenizer, labels, pad_token_label_id, mode):
         features = torch.load(cached_features_file)
     else:
         print("Creating features from dataset file at", args.data_dir)
-        examples = read_examples_from_file(args.data_dir, mode)
+        examples = read_examples_from_file(args,args.data_dir, mode)
         features = convert_examples_to_features(
             examples,
             labels,
